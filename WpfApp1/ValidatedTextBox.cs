239daf5a16ab2f73852a3d7fcf5d9228f00a6e3f
Mayur Banta <mayurbanta@gmail.com>
2019-07-12 11:00:37 +0530

    Add project files.

---
+++ WpfApp1/ValidatedTextBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public class ValidatedTextBox : TextBox
+    {
+
+        public ValidatedTextBox()
+        {
+
+        }
+
+        public bool IsSpaceAllowed { get; set; }
+
+        //public bool IsSpaceAllowed
+        //{
+        //    get { return (bool)GetValue(IsSpaceAllowedProperty); }
+        //    set { SetValue(IsSpaceAllowedProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for IsSpaceAllowed.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty IsSpaceAllowedProperty =
+        //    DependencyProperty.Register("IsSpaceAllowed", typeof(bool), typeof(ValidatedTextBox));
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            base.OnPreviewKeyDown(e);
+            if (!IsSpaceAllowed && (e.Key == Key.Space))
+            {
+                e.Handled = true;
+            }
+        }
+
+        
+
+
+    }
+}
